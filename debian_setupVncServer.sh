54cc1f7d774a1d6feead2e09ad374c97ea8e43b8
Cody Garrett <cody@garrett.ms>
2020-01-29 11:50:56 -0500

    Create debian_setupVncServer.sh

---
+++ debian_setupVncServer.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+######################################
+###Requires Sudo installation first###
+######################################
+
+randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8};echo;}
+sudo apt-get install -y tightvncserver
+
+VNC_USERNAME="cody"
+VNC_PASSWORD=$(randpw)
+
+rm -Rf /home/$VNC_USERNAME/.vnc
+mkdir /home/$VNC_USERNAME/.vnc
+echo $VNC_PASSWORD | vncpasswd -f > /home/$VNC_USERNAME/.vnc/passwd
+chown -R $VNC_USERNAME:$VNC_USERNAME /home/$VNC_USERNAME/.vnc
+chmod 0600 /home/$VNC_USERNAME/.vnc/passwd
+
+vncserver 
+
+echo "Password for VNC is $VNC_PASSWORD"
+echo "$VNC_PASSWORD" > ~/vnc_password.txt
+vncserver -kill :1
+
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+
+echo "#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &" > ~/.vnc/xstartup
+
+chmod +x ~/.vnc/xstartup
+
+sudo echo "[Unit]
+Description=Start TightVNC server at startup
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=$VNC_USERNAME
+Group=$VNC_USERNAME
+WorkingDirectory=/home/$VNC_USERNAME
+
+PIDFile=/home/$VNC_USERNAME/.vnc/%H:%i.pid
+ExecStartPre=-/usr/bin/vncserver -kill :%i > /dev/null 2>&1
+ExecStart=/usr/bin/vncserver -depth 24 -geometry 1280x800 :%i
+ExecStop=/usr/bin/vncserver -kill :%i
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/vncserver@.service 
+
+sudo systemctl daemon-reload
+sudo systemctl enable vncserver@1.service
+
+vncserver -kill :1
+
+sudo systemctl start vncserver@1
+sudo systemctl status vncserver@1
